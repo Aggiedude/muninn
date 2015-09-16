30ebe8c409c509cbfbf87499d35088a244f8e05f
Andrew Chepey <achepey@gmail.com>
2015-09-16 15:04:15 -0500

    Initial Commit

---
+++ app/src/androidTest/java/flycam/csce_483/muninn/ApplicationTest.java
@@ -0,0 +1,13 @@
+package flycam.csce_483.muninn;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
