eae0e8b02e454ef910aa34c8ac29b7221c6d467d
Andrew <achepey@gmail.com>
2015-10-29 16:30:28 -0500

    Added camera UI functionality, still needs HTTP requests to work

--- app/src/main/java/flycam/csce_483/muninn/CameraActivity.java
+++ app/src/main/java/flycam/csce_483/muninn/CameraActivity.java
@@ -2,6 +2,7 @@ package flycam.csce_483.muninn;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.net.Uri;
@@ -9,8 +10,24 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.VideoView;
-import android.widget.MediaController;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class CameraActivity extends Activity {
 
@@ -18,7 +35,8 @@ public class CameraActivity extends Activity {
     ImageButton fabRecord;
     ImageButton fabStopRecord;
 
-    boolean recording;
+    private boolean mode; // true is camera mode, false is video mode
+    private boolean recording;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +47,8 @@ public class CameraActivity extends Activity {
 
         setContentView(R.layout.activity_camera);
 
+        connectToCamera();
+
         VideoView videoView = (VideoView) findViewById(R.id.liveVideo);
 
         String videoURL = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
@@ -36,7 +56,7 @@ public class CameraActivity extends Activity {
 
         videoView.setVideoURI(vidUri);
 
-
+        final ImageView recordingStatus = (ImageView) findViewById(R.id.recordingStatus);
         fabCamera = (ImageButton) findViewById(R.id.cameraButtonFAB);
         fabRecord = (ImageButton) findViewById(R.id.recordButtonFAB);
         fabStopRecord = (ImageButton) findViewById(R.id.stopButtonFAB);
@@ -44,23 +64,100 @@ public class CameraActivity extends Activity {
         fabCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ;// send request to take picture on goPro
+                Log.d("camera", "mode = " + mode);
+                Log.d("camera", "recording = " + recording);
+
+                if (!mode && !recording) {
+                    mode = true;
+                    Log.d("camera", "Made it through to mode switch");
+                    String response = sendRequest("10.5.5.9/gp/gpControl/command/mode?p=1"); // switches to camera mode
+                    Log.d("camera", response);
+                }
+                if (mode && !recording) {
+                    Log.d("camera", "Made it through to shutter switch");
+                    sendRequest("10.5.5.9/gp/gpControl/command/shutter?p=1");// send request to take picture on goPro
+                }
             }
         });
 
         fabRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ;// send request to start recording on goPro
+                if(mode){
+                    mode = false;
+                    sendRequest("10.5.5.9/gp/gpControl/command/mode?p=0"); // switches to video mode
+                }
+                if (!mode && !recording) {
+                    sendRequest("10.5.5.9/gp/gpControl/command/shutter?p=1");// send request to start recording on goPro
+                    recording = true;
+                    fabRecord.setVisibility(View.INVISIBLE);
+                    fabStopRecord.setVisibility(View.VISIBLE);
+                    recordingStatus.setVisibility(View.VISIBLE);
+                }
             }
         });
 
-        fabCamera.setOnClickListener(new View.OnClickListener() {
+        fabStopRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ;// send request to stop recording on goPro
+                sendRequest("10.5.5.9/gp/gpControl/command/shutter?p=0");// send request to stop recording on goPro
+                recording = false;
+                fabRecord.setVisibility(View.VISIBLE);
+                fabStopRecord.setVisibility(View.INVISIBLE);
+                recordingStatus.setVisibility(View.INVISIBLE);
             }
         });
+    }
+
+    private void connectToCamera() {
+
+        String URL = "10.5.5.9/gp/gpControl/command/mode?p=0";
+
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response = httpclient.execute(new HttpGet(URL));
+            StatusLine statusLine = response.getStatusLine();
+            if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                String responseString = out.toString();
+                out.close();
+            } else {
+                //Closes the connection.
+                response.getEntity().getContent().close();
+            }
+        }
+        catch(IOException e){
+            Log.e("Camera", "Connection to camera failed");
+        }
+
+    }
+
+    private String sendRequest(String s) {
+
+        String response = "";
+        try {
+            URL url = new URL(s);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            response = readStream(in);
+            urlConnection.disconnect();
+        }
+        catch(IOException e) {
+            return "";
+        }
+
+        return response;
+    }
+
+    private String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        for (String line = r.readLine(); line != null; line =r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        return sb.toString();
     }
 
     @Override
